e34900a9f35b1d2272264d7028c69bdec0bc0232
Anne Hartebrodt <anne.online@posteo.net>
2026-05-06 15:39:25 +0200

    add alternet wrapper object (up to D disamb)

--- src/alternet/alternet.py
+++ src/alternet/alternet.py
@@ -21,7 +21,8 @@ class Alternet:
         'MIN_FREQUENCY',
         'IMPORTANCE_PERCENTILE',
         'regulator_list',
-        
+        'gene2tx',
+        'tx2gene'
     ]
 
     def __init__(
@@ -47,16 +48,18 @@ class Alternet:
         self.IMPORTANCE_PERCENTILE = 0.7
 
         self.gene_data, self.sample_cols = self._compute_gene_counts()
+        self.gene2tx = self._gene2tx()
+        self.tx2gene = self._tx2gene()
         self.usage_df, self.reliability_df = self._calculate_transcript_usage()
         self.canonical, self. as_source, self.as_full = self._filter_edges()
         self.set_a = self._compute_set_a()
         self.set_d_full = self._compute_set_d_full()
         self.set_d = self._compute_set_d()
+        self.set_b = self._compute_set_b()
+        self.set_c = self._compute_set_c()
 
 
-        self.sample_cols = []
-        self.set_b = None
-        self.set_c = None
+
 
 
 
@@ -65,6 +68,16 @@ class Alternet:
         sample_cols = [c for c in self.transcript_data.columns if c not in [self.transcript_col, self.gene_col]]
         gene_tpm = self.transcript_data.groupby('gene_id')[sample_cols].sum().reset_index()
         return gene_tpm, sample_cols
+
+        
+    def _gene2tx(self):
+        gene_to_transcripts = self.transcript_data.groupby(self.gene_col)[self.transcript_col].apply(list).to_dict()
+        return gene_to_transcripts
+
+    def _tx2gene(self):
+        tx2gene =  dict(zip(self.transcript_data[self.transcript_col], self.transcript_data[self.gene_col]))
+        return tx2gene
+
 
     def _filter_edges(self):
         canonical_grn_filtered = filter_edges(self.canonical, self.MIN_FREQUENCY, self.IMPORTANCE_PERCENTILE)
@@ -121,3 +134,34 @@ class Alternet:
         # Sort by median importance
         set_d = set_d.sort_values('median_importance', ascending=False)
         return set_d
+
+    def _compute_set_b(self):
+        print('Computeing set B')
+        # Net2: ALL TF-like edges (TF + TF_SF) - they all act as TFs in gene-level target network
+        net2_for_t2 = self.as_source[self.as_source['reg_type'].isin(['TF', 'TF_SF'])].copy()
+        # Net3: TF only (TF_SF is handled via Set D)
+        net3_tf_only = self.as_full[self.as_full['reg_type'] == 'TF'].copy()
+        # Add the ones from set d which are tf edges
+        tfsf_tf_like = self.set_d_full[self.set_d_full['tfsf_category'] == 'tfsf_tf_like'].copy()
+        
+        net3_tfsf = self.as_full[self.as_full['reg_type'] == 'TF_SF'].copy()
+        set_b = as_source_vs_as_full(net2_for_t2, net3_tf_only, tfsf_tf_like, net3_tfsf, self.tx2gene)
+        print(set_b)
+        set_b_unpacked = unpack_set_b(net3_tf_only, tfsf_tf_like, net3_tfsf, set_b)
+        print(set_b_unpacked)
+        set_b_unpacked = annotate_set_b(set_b,set_b_unpacked)
+        print(set_b_unpacked)
+        return set_b_unpacked
+
+    def _compute_set_c(self):
+        net3_tfsf = self.as_full[self.as_full['reg_type'] == 'TF_SF'].copy()
+        tfsf_sf_like = self.set_d_full[self.set_d_full['tfsf_category'] == 'tfsf_sf_like'].copy()
+        net3_sf = self.as_full[self.as_full['reg_type'] == 'SF'].copy()
+        udf = self.usage_df.drop(columns = {'gene_id'}).set_index("transcript_id")
+        t_temps = self.transcript_data.drop(columns = {self.gene_col}).set_index(self.transcript_col)
+
+        set_c = compute_set_c(net3_sf, t_temps, self.gene2tx, udf, self.reliability_df, tfsf_sf_like, self.sample_cols, net3_tfsf)
+        set_c_unpacked = unpack_set_c(net3_sf, tfsf_sf_like, net3_tfsf, set_c)
+        set_c = annotate_set_c(set_c, set_c_unpacked)
+        return set_c
+        
